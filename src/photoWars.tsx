6a01972f8e86d0fce65b180025510745c566026e
JackCHG <gongjackiegong@gmail.com>
2023-04-24 00:36:07 -0700

    Added all ts and tsx files

---
+++ src/photoWars.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PhotoWars extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Hello World!</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<PhotoWars />, document.getElementById('photowarsapp'));
