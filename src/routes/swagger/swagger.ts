a57011f127d510dc2908b994ebee12431fd9a0d2
JackCHG <gongjackiegong@gmail.com>
2023-04-26 14:36:01 -0700

    Changed swagger documentation to update automatically with comments

---
+++ src/routes/swagger/swagger.ts
@@ -0,0 +1,33 @@
+"use strict"
+
+// https://blog.logrocket.com/documenting-express-js-api-swagger/
+import express = require('express');
+import swaggerUi = require('swagger-ui-express');
+import swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CS 194 Express API with Swagger",
+      version: "1.0",
+    }
+  },
+  apis: ["./server.ts", "./routes/**/*.ts"]
+};
+const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerRouter = express.Router();
+
+/**
+ * @openapi
+ * /swagger:
+ *   get:
+ *     summary: Returns OpenAPI specification of APIs.
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of OpenAPI specification.
+ */
+swaggerRouter.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+export { swaggerRouter };
