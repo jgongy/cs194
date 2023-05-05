b74896dbf663ed1e492d76726e0fc1cac7e8b0a4
JackCHG <gongjackiegong@gmail.com>
2023-05-05 02:06:11 -0700

    Removed override rule in eslint

--- src/.eslintrc.cjs
+++ src/.eslintrc.cjs
@@ -3,21 +3,5 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  root: true,
-  overrides: [
-    {
-      files: ['**/*.tsx'],
-      rules: {
-        '@typescript-eslint/ban-types': [
-          'error',
-          {
-            extendDefaults: true,
-            types: {
-              '{}': false
-            }
-          }
-        ]
-      }
-    }
-  ]
+  root: true
 };
