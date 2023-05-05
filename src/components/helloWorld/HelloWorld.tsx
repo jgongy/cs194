795ec663dc064fd0e1bd6604b3979f3ce08d0b2a
JackCHG <gongjackiegong@gmail.com>
2023-05-05 01:47:50 -0700

    Changed React class Components to functional Components

--- src/components/helloWorld/HelloWorld.tsx
+++ src/components/helloWorld/HelloWorld.tsx
@@ -1,55 +1,45 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
 import axios from 'axios';
 
-interface State {
-  buttonText: string
-}
+const HelloWorld = () => {
+  const [buttonText, setButtonText] = useState('Ping');
 
-class HelloWorld extends React.Component<{}, State> {
-  constructor(props) {
-    super(props);
-    this.state = { buttonText: 'Ping' };
-  }
+  const clickLearnMore = async () => {
+    const path = '/test/ping';
+    try {
+      const res = await axios.get(path);
+      setButtonText(res.data);
+    } catch (err) {
+      console.error('Failed to get path', err);
+    }
+  };
 
-  clickLearnMore() {
-    const path = '/test/ping'
-    const promise = axios.get(path);
-    promise.then((res) => {
-      this.setState({ buttonText: res.data});
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Phrase of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          Hello World!
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          greeting
+        </Typography>
+        <Typography variant="body2">
+          a common phrase used in computer programs.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={() => clickLearnMore()}>{buttonText}</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-  render() {
-    return (
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Phrase of the Day
-          </Typography>
-          <Typography variant="h5" component="div">
-            Hello World!
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            greeting
-          </Typography>
-          <Typography variant="body2">
-            a common phrase used in computer programs.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" onClick={() => this.clickLearnMore()}>{this.state.buttonText}</Button>
-        </CardActions>
-      </Card>
-    );
-  }
-}
-
-export default HelloWorld;
+export { HelloWorld };
--- src/photoWars.tsx
+++ src/photoWars.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 /* Importing Components */
-import HelloWorld from './components/helloWorld/HelloWorld';
+import { HelloWorld } from './components/helloWorld/HelloWorld';
 
-class PhotoWars extends React.Component {
-  render() {
-    return (
-      <React.StrictMode>
-        <HelloWorld />
-      </React.StrictMode>
-    );
-  }
-}
+const PhotoWars = () => {
+  return (
+    <React.StrictMode>
+      <HelloWorld />
+    </React.StrictMode>
+  );
+};
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(<PhotoWars />);
