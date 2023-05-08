52448d0b4c79bad4edcfe4e1912ae42b0be7e00e
Grace <gkzhou@stanford.edu>
2023-05-07 21:02:53 -0700

    Created form for creating a war

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { PhotoCard } from '../PhotoCard';
 import Grid from '@mui/material/Grid';
 
+import { Create } from '../create/Create';
+
 const Feed = () => {
   return (
     <Grid>
       <PhotoCard />
+      <Create />
     </Grid>
   );
 };
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const Sidebar = () => {
   return (
     <div>
-      Sidebar
+      <Button>Create war</Button>
     </div>
   );
 };
