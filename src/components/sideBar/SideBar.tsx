6691aaeabf1bfe25bd777578158fc947c2b38e16
JackCHG <gongjackiegong@gmail.com>
2023-05-11 11:35:31 -0700

    Added correct sidebar to home page

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -8,38 +8,32 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 
-class SideBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-    <ProSidebarProvider>
-      <div id="app" style={({ height: "100vh", display: "flex" })}>
-        <Sidebar style={{ height: "100vh" }}>
-        <Menu>
-          <MenuItem>
-            <h2>PhotoWars</h2>
-          </MenuItem>
-    
-          <MenuItem icon={<ExploreOutlinedIcon />}>Explore</MenuItem>
-          <MenuItem icon={<PeopleOutlinedIcon />}>Open Competitions</MenuItem>
-          <MenuItem icon={<EmojiEventsOutlinedIcon />}>Winners</MenuItem>
-          <MenuItem icon={<NotificationsOutlinedIcon />}>Notifications</MenuItem>
-          <MenuItem icon={<SearchOutlinedIcon />}>Search</MenuItem>
-          <MenuItem icon={<AccountCircleOutlinedIcon />}>Profile</MenuItem>
-        </Menu>
-        </Sidebar>
-        <main>
-        <h1 style={{ color: "white", marginLeft: "5rem" }}>
-          React-Pro-Sidebar
-        </h1>
-        </main>
-      </div>
-    </ProSidebarProvider>
-    );
-  }
+const SideBar = () => {
+  return (
+  <ProSidebarProvider>
+    <div id="app" style={({ height: "100vh", display: "flex" })}>
+      <Sidebar style={{ height: "100vh" }}>
+      <Menu>
+        <MenuItem>
+          <h2>PhotoWars</h2>
+        </MenuItem>
+  
+        <MenuItem icon={<ExploreOutlinedIcon />}>Explore</MenuItem>
+        <MenuItem icon={<PeopleOutlinedIcon />}>Open Competitions</MenuItem>
+        <MenuItem icon={<EmojiEventsOutlinedIcon />}>Winners</MenuItem>
+        <MenuItem icon={<NotificationsOutlinedIcon />}>Notifications</MenuItem>
+        <MenuItem icon={<SearchOutlinedIcon />}>Search</MenuItem>
+        <MenuItem icon={<AccountCircleOutlinedIcon />}>Profile</MenuItem>
+      </Menu>
+      </Sidebar>
+      <main>
+      <h1 style={{ color: "white", marginLeft: "5rem" }}>
+        React-Pro-Sidebar
+      </h1>
+      </main>
+    </div>
+  </ProSidebarProvider>
+  );
 }
 
-export default SideBar;
+export { SideBar };
