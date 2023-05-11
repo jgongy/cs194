91e5cf48afbf316487a2b0b9ad170c6853ccd357
JackCHG <gongjackiegong@gmail.com>
2023-05-10 21:59:27 -0700

    Modified webpack config and package.json

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -39,7 +39,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
+    publicPath: '/',
     filename: '[name].bundle.js',
   },
   devServer: {
