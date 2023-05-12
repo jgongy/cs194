c28c1a9953433416e2ebfc70fe59d4ed548b0c53
JackCHG <gongjackiegong@gmail.com>
2023-05-12 05:59:18 -0700

    Changed Home page layout

--- src/react/pages/home/Home.tsx
+++ src/react/pages/home/Home.tsx
@@ -5,23 +5,25 @@ import { RightBar } from '../../components/rightBar/RightBar';
 import { SideBar } from '../../components/sideBar/SideBar';
 import { TopBar } from '../../components/topBar/TopBar';
 import { Outlet } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
+import {
+  Grid,
+  Stack
+}from '@mui/material';
 
 const Home = () => {
   return (
     <ProSidebarProvider>
       <div style={({ height: "100vh", display: "flex" })}>
-        <Grid container wrap="nowrap" spacing={7}>
-          <Grid item xs={1} />
-          <Grid item>
-            <SideBar breakpoint="sm" />
+        <Stack direction="row">
+          <SideBar breakpoint="sm" />
+          <Grid container spacing={7}>
+            <Grid item xs={1} md={2} />
+            <Grid item xs={7} s={6} md={6} lg={4} xl={5}>
+              <Outlet />
+            </Grid>
+            <Grid item xs={1} md={2} xl={4} />
           </Grid>
-          <Grid item xs={1} md={2} />
-          <Grid item xs={7} s={6} md={6} lg={4} xl={5}>
-            <Outlet />
-          </Grid>
-          <Grid item xs={1} md={2} xl={4} />
-        </Grid>
+        </Stack>
       </div>
     </ProSidebarProvider>
   );
