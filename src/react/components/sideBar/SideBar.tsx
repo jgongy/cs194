d556071b4cb4f044551a0f8b18892f31fcf83ffb
JackCHG <gongjackiegong@gmail.com>
2023-05-12 18:40:47 -0700

    Fixed sidebar when scrolling through posts

--- src/react/components/sideBar/SideBar.tsx
+++ src/react/components/sideBar/SideBar.tsx
@@ -15,57 +15,69 @@ import {
   MenuItem,
   Sidebar,
   useProSidebar
-} from "react-pro-sidebar";
+} from 'react-pro-sidebar';
 
-const SideBar = () => {
+const SideBar = ({}) => {
   const [loginOpen, setLoginOpen] = useState(false);
   const [registerOpen, setRegisterOpen] = useState(false);
-
-  const { collapseSidebar } = useProSidebar();
+  const {
+    collapseSidebar,
+    toggleSidebar,
+    collapsed,
+    toggled
+  } = useProSidebar();
 
   return (
-      <Sidebar style={{ height: "100vh" }}>
-      <Menu>
-        <MenuItem
-          icon={<MenuOutlinedIcon />}
-          onClick={() => {
-            collapseSidebar();
-          }}
-          style={{textAlign: "center" }}
+    <Sidebar
+      style={{
+        position: '-webkit-sticky',
+        position: 'sticky',
+        top: 0,
+        height: '100vh',
+      }}
+    >
+    <Menu>
+      <MenuItem
+        icon={<MenuOutlinedIcon />}
+        onClick={() => {
+          collapseSidebar();
+          toggleSidebar();
+        }}
+        style={{textAlign: "center" }}
+      >
+        <Typography variant="h5">Photo Wars</Typography>
+      </MenuItem>
+      <MenuItem icon={<ExploreOutlinedIcon />}>Explore</MenuItem>
+      <MenuItem icon={<PeopleOutlinedIcon />}>Open Competitions</MenuItem>
+      <MenuItem icon={<EmojiEventsOutlinedIcon />}>Winners</MenuItem>
+      <MenuItem icon={<NotificationsOutlinedIcon />}>Notifications</MenuItem>
+      <MenuItem icon={<SearchOutlinedIcon />}>Search</MenuItem>
+      <MenuItem icon={<AccountCircleOutlinedIcon />}>Profile</MenuItem>
+      <MenuItem component="div" style={{ cursor: 'default'}}>
+        <Button component={Link}
+          to="/create"
+          startIcon={<AddCircleOutlineIcon/>}
+          variant="outlined"
         >
-          <Typography variant="h5">Photo Wars</Typography>
-        </MenuItem>
-        <MenuItem icon={<ExploreOutlinedIcon />}>Explore</MenuItem>
-        <MenuItem icon={<PeopleOutlinedIcon />}>Open Competitions</MenuItem>
-        <MenuItem icon={<EmojiEventsOutlinedIcon />}>Winners</MenuItem>
-        <MenuItem icon={<NotificationsOutlinedIcon />}>Notifications</MenuItem>
-        <MenuItem icon={<SearchOutlinedIcon />}>Search</MenuItem>
-        <MenuItem icon={<AccountCircleOutlinedIcon />}>Profile</MenuItem>
-        <MenuItem component="div" style={{ cursor: 'default'}}>
-          <Button component={Link}
-            to="/create"
-            startIcon={<AddCircleOutlineIcon/>}
-            variant="outlined"
-          >
-            Create Battle
-          </Button>
-        </MenuItem>
-        <MenuItem>
-          <Grid container wrap="nowrap" spacing={1}>
-            <Grid item>
-              <Button
-                onClick={() => setLoginOpen(true)}
-                variant="outlined"
-              >Login</Button>
-            </Grid>
-            <Grid item>
-              <Button variant="outlined">Register</Button>
-            </Grid>
+          Create Battle
+        </Button>
+      </MenuItem>
+      <MenuItem>
+        <Grid container wrap="nowrap" spacing={1}>
+          <Grid item>
+            <Button
+              onClick={() => setLoginOpen(true)}
+              variant="outlined"
+            >Login</Button>
           </Grid>
-        </MenuItem>
-      </Menu>
-      <LoginModal loginOpen={loginOpen} setLoginOpen={setLoginOpen} />
-      </Sidebar>
+          <Grid item>
+            <Button variant="outlined">Register</Button>
+          </Grid>
+        </Grid>
+      </MenuItem>
+    </Menu>
+    <LoginModal loginOpen={loginOpen} setLoginOpen={setLoginOpen} />
+    </Sidebar>
   );
 }
 
--- src/react/pages/home/Home.tsx
+++ src/react/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Feed } from '../../components/feed/Feed';
-import { ProSidebarProvider } from 'react-pro-sidebar';
 import { RightBar } from '../../components/rightBar/RightBar';
 import { SideBar } from '../../components/sideBar/SideBar';
 import { TopBar } from '../../components/topBar/TopBar';
@@ -8,24 +7,26 @@ import { Outlet } from 'react-router-dom';
 import {
   Grid,
   Stack
-}from '@mui/material';
+} from '@mui/material';
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import './home.css';
 
 const Home = () => {
+
   return (
-    <ProSidebarProvider>
-      <div style={({ height: "100vh", display: "flex" })}>
-        <Stack direction="row">
-          <SideBar breakpoint="sm" />
-          <Grid container spacing={7}>
-            <Grid item xs={1} md={2} />
-            <Grid item xs={7} s={6} md={6} lg={4} xl={5}>
-              <Outlet />
-            </Grid>
-            <Grid item xs={1} md={2} xl={4} />
-          </Grid>
-        </Stack>
-      </div>
-    </ProSidebarProvider>
+    <Stack direction="row">
+      <ProSidebarProvider >
+        <SideBar />
+      </ProSidebarProvider>
+
+      <Grid container spacing={7}>
+        <Grid item xs={1} md={2} />
+        <Grid item xs={7} s={6} md={6} lg={4} xl={5}>
+          <Outlet />
+        </Grid>
+        <Grid item xs={1} md={2} xl={4} />
+      </Grid>
+    </Stack>
   );
 };
 
