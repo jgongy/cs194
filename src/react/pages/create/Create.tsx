3adbbdb961c229d1ba48a23fa63ff337771eefe8
Grace <gkzhou@stanford.edu>
2023-05-14 02:57:19 -0700

    Revised Create War form to send to endpoint

--- src/react/pages/create/Create.tsx
+++ src/react/pages/create/Create.tsx
@@ -5,88 +5,72 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import './create.css';
 import axios from "axios";
+import { Controller, useForm } from 'react-hook-form';
 
 const Create = () => {
-  const [title, setTitle] = useState('');
   const [image, setImage] = useState(null);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    axios.post(`/battle/${id}/submit:`, {
-      title: title,
-      image: image
-    })
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }
-
-  function handleTitleChange(event) {
-    setTitle(event.target.value);
-  }
-
-  function handleImageChange(event) {
-    const file = event.target.files[0];
-    setImage(file);
-  }
-
-  function handleImageDrop(event) {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    setImage(file);
-  }
+  const { control,
+    handleSubmit,
+  } = useForm({ mode: 'onChange' });
+  const handleFormSubmit = async (data) => {
+    console.log(data);
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("file", image);
+    try {
+      const res = await axios.post('/battle/new', formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
 
   return (
     <div>
       <Typography variant="h6">
         Create a war
       </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          fullWidth
-          id="outlined-basic"
-          label="Title"
-          variant="outlined"
-          value={title}
-          onChange={handleTitleChange}
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <Controller
+          name="title"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Title"
+              variant="outlined"
+              {...field}
+            />
+          )}
         />
+              {image ? (
+                <img src={URL.createObjectURL(image)} className="image-preview" />
+              ) : (
+                <Box
+                  onDragOver={(event) => event.preventDefault()}
+                  sx={{ p: 2, border: "1px dashed grey" }}
+                >
+                  <label htmlFor="image-upload">Drag and drop an image, or</label>
 
-        {image ? (
-          <img
-            src={URL.createObjectURL(image)}
-            className='image-preview'
-          />)
-          : (
-            <Box
-              // className="image-upload-box"
-              onDrop={handleImageDrop}
-              onDragOver={(event) => event.preventDefault()}
-              sx={{ p: 2, border: '1px dashed grey' }}
-            >
-              <label htmlFor="image-upload">
-                Drag and drop an image, or
-              </label>
-
-              <Button
-                variant="outlined"
-                component="label"
-              >
-                Upload File
-                <input
-                  type="file"
-                  hidden
-                  id="image-upload"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                />
-              </Button>
-
-            </Box>
-          )
-        }
+                  <Button variant="outlined" component="label">
+                    Upload File
+                    <input
+                      type="file"
+                      hidden
+                      id="image-upload"
+                      accept="image/*"
+                      name="image"
+                      onChange={(event) => setImage(event.target.files[0])}
+                    />
+                  </Button>
+                </Box>
+              )}
         <Button
           variant="contained"
           type="submit"
