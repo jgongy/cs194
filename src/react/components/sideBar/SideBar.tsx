8b59ce73a1ef795c63304b22db7e436f04ff4ca6
JackCHG <gongjackiegong@gmail.com>
2023-05-26 04:40:42 -0700

    Revamping sidebar and topbar

--- src/react/components/sideBar/SideBar.tsx
+++ src/react/components/sideBar/SideBar.tsx
@@ -2,19 +2,22 @@ import React, { useContext } from 'react';
 import axios from 'axios';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-// import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
 import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined';
-// import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import { Button, Typography } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
 import {
-  Menu,
-  MenuItem,
-  Sidebar,
-  useProSidebar
-} from 'react-pro-sidebar';
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  Toolbar,
+  Typography
+} from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 import { LoginModal } from '../../components/loginModal/LoginModal';
 import { UserContext } from '../../contexts/UserContext';
 
@@ -25,88 +28,42 @@ const SideBar = () => {
     userId,
     setUserId
   } = useContext(UserContext);
-  const {
-    broken,
-  } = useProSidebar();
 
   const navigate = useNavigate();
-
-  const handleLogOut = async () => {
-    const path = '/account/logout';
-    try {
-      await axios.post(path);
-      setDisplayName('');
-      setUserId('');
-      localStorage.removeItem('user');
-      navigate('/');
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  const drawerWidth = 240;
 
   return (
-    <Sidebar
-      style={{
-        ...!broken && {
-          position: 'sticky',
-          top: 0
-        },
-        height: '100vh'
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
       }}
-      breakPoint="md"
     >
-      <Menu
-        menuItemStyles={{
-          button: {
-            '&:hover': {
-              textDecoration: 'underline'
-            }
+      <Toolbar />
+      <Box sx={{ overflow: 'auto' }}>
+        <List>
+          <ListItem key={'explore'} disablePadding>
+            <ListItemButton onClick={() => navigate('/')}>
+              <ListItemIcon>
+                <ExploreOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary={'Explore'} />
+            </ListItemButton>
+          </ListItem>
+          { userId &&
+            <ListItem key={'profile'} disablePadding>
+              <ListItemButton onClick={() => navigate('/')}>
+                <ListItemIcon>
+                  <AccountCircleOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary={displayName} />
+              </ListItemButton>
+            </ListItem>
           }
-        }}
-      >
-        <MenuItem onClick={() => navigate('/')}>
-            <Typography variant="h5">Photo Wars</Typography>
-        </MenuItem>
-        <MenuItem onClick={() => navigate('/')} icon={<ExploreOutlinedIcon />}>
-            Explore
-        </MenuItem>
-        {/*
-        <MenuItem icon={<PeopleOutlinedIcon />}>Open Competitions</MenuItem>
-        <MenuItem icon={<EmojiEventsOutlinedIcon />}>Winners</MenuItem>
-        <MenuItem icon={<NotificationsOutlinedIcon />}>Notifications</MenuItem>
-        <MenuItem icon={<SearchOutlinedIcon />}>Search</MenuItem>
-        */}
-        {
-          userId
-          && <MenuItem
-               icon={<AccountCircleOutlinedIcon />}
-               onClick={() => navigate(`/users/${userId}`)}
-             >
-               {displayName}
-             </MenuItem>
-        }
-        {userId === null ? <div /> : userId !== '' ?
-          <React.Fragment>
-            <MenuItem component="div" style={{ cursor: 'default' }}>
-              <Button component={Link}
-                to="/create"
-                startIcon={<AddCircleOutlineIcon />}
-                variant="outlined"
-              >
-                Create War
-              </Button>
-            </MenuItem>
-            <MenuItem component="div" style={{ cursor: 'default' }}>
-              <Button onClick={handleLogOut} variant="outlined">
-                Log Out
-              </Button>
-            </MenuItem>
-          </React.Fragment>
-          :
-          <LoginModal />
-        }
-      </Menu>
-    </Sidebar>
+        </List>
+      </Box>
+    </Drawer>
   );
 }
 
--- src/react/components/topBar/TopBar.tsx
+++ src/react/components/topBar/TopBar.tsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import axios from 'axios';
 import {
-} from '@mui/material/Button';
+  AppBar,
+  Box,
+  Button,
+  Stack,
+  Toolbar,
+  Typography
+} from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { UserContext } from '../../contexts/UserContext';
+import { LoginModal } from '../loginModal/LoginModal';
+import { Link, useNavigate } from 'react-router-dom';
 
 const TopBar = () => {
+  const {
+    displayName,
+    setDisplayName,
+    userId,
+    setUserId
+  } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    const path = '/account/logout';
+    try {
+      await axios.post(path);
+      setDisplayName('');
+      setUserId('');
+      localStorage.removeItem('user');
+      navigate('/');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
-    <div>Home</div>
+    <AppBar position="fixed" sx={{ backgroundColor: "grey", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <Toolbar>
+        <Stack
+          alignItems="center"
+          direction="row"
+          justifyContent="space-between"
+          sx={{ width: '100%' }}
+        >
+          <Typography>PhotoWars</Typography>
+          <Box>
+            {userId === null ? <div /> : userId !== '' ?
+              <React.Fragment>
+                  <Button
+                    component={Link}
+                    startIcon={<AddCircleOutlineIcon />}
+                    to="/create"
+                    variant="contained"
+                  >
+                    Create War
+                  </Button>
+                  <Button variant="contained" onClick={handleLogOut}>
+                    Log Out
+                  </Button>
+              </React.Fragment>
+              :
+              <LoginModal />
+            }
+          </Box>
+
+        </Stack>
+      </Toolbar>
+    </AppBar>
   );
 };
 
