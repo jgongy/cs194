ce61af492b72b173b9f2c2c37aba9b4d71d525e3
JackCHG <gongjackiegong@gmail.com>
2023-06-10 12:38:40 -0700

    Updated battle dummy data deadlines

--- src/development/battles/00-skullcat/battle.ts
+++ src/development/battles/00-skullcat/battle.ts
@@ -3,7 +3,7 @@ import { fakeUsers } from "../../users";
 const battle = {
   author: fakeUsers[0]!._id,
   caption: 'This cat with a skull on his chest',
-  deadline: '2023-06-20T00:26:13.696Z',
+  deadline: '2023-06-25T00:26:13.696Z',
   votes: [fakeUsers[1]]
 };
 
--- src/development/battles/06-catsHugging/battle.ts
+++ src/development/battles/06-catsHugging/battle.ts
@@ -3,7 +3,7 @@ import { fakeUsers } from "../../users";
 const battle = {
   author: fakeUsers[3]!._id,
   caption: 'catsHugging',
-  deadline: '2023-06-10T00:26:13.696Z',
+  deadline: '2023-06-11T00:26:13.696Z',
   votes: []
 };
 
